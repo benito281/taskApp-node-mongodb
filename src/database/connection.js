782dee2f573283d501322dad25a2c7c93a2f2031
benito281 <benitoortiz281@gmail.com>
2022-01-17 17:28:54 -0300

    Modificado

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,7 +4,7 @@ const MONGO_ATLAS = process.env.MONGODB_ATLAS;
 (
     async () => {
         try {
-            const db = await connect('mongodb://localhost/task_app');
+            const db = await connect(MONGO_ATLAS);
             console.log('Database is connected:',db.connection.name);
         } catch (error) {
             console.log(error);
